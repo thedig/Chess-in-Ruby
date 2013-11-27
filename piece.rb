7da2fcca84b67493d0fdde9573afb9804ca9c841
enquea <student@appacademy.io>
2013-11-26 17:29:06 -0800

    added unicode chars

--- piece.rb
+++ piece.rb
@@ -1,3 +1,4 @@
+# encoding: utf-8
 require_relative './board.rb'
 
 class MethodUndefinedError < StandardError
@@ -51,21 +52,27 @@ class Pawn < Piece
   end
 
   def valid_moves
-    offsets = [[1,0], [1, -1], [1,1]]
+    offsets = [[1,0], [1, -1], [1,1], [2,0]]
 
     potential_moves = offsets.map! do |coord|
       [(coord[0] * @direction) + @position[0], (coord[1] + @position[1])]
     end
-
     potential_moves.select! { |coord| position_in_bounds?(coord) }
 
     all_valid_moves = []
+    next_move_open = false
 
     potential_moves.each_with_index do |move, index|
       current_piece = @board[move[0], move[1]]
       case index
       when 0
-        all_valid_moves << move if current_piece.nil?
+        if current_piece.nil?
+          all_valid_moves << move
+          next_move_open = true
+        end
+      when 3
+        all_valid_moves << move if
+          (current_piece.nil? && on_beginning_line?) && next_move_open
       else
         all_valid_moves << move if !current_piece.nil? && current_piece.color != @color
       end
@@ -74,7 +81,9 @@ class Pawn < Piece
   end
 
   def to_s
-    "[P#{color}]"
+    #"[P#{color}]"
+     @color == :b ? "[♟ ]" : "[♙ ]"
+
   end
 
 end
@@ -111,7 +120,8 @@ class King < SteppingPiece
   end
 
   def to_s
-    "[K#{color}]"
+    #"[K#{color}]"
+    @color == :b ? "[♚ ]" : "[♔ ]"
   end
 
 end
@@ -122,7 +132,8 @@ class Knight < SteppingPiece
   end
 
   def to_s
-    "[N#{color}]"
+    #"[N#{color}]"
+    @color == :b ? "[♞ ]" : "[♘ ]"
   end
 end
 
@@ -162,7 +173,8 @@ class Rook < SlidingPiece
   end
 
   def to_s
-    "[R#{color}]"
+    #"[R#{color}]"
+    @color == :b ? "[♜ ]" : "[♖ ]"
   end
 
 end
@@ -174,7 +186,8 @@ class Queen < SlidingPiece
   end
 
   def to_s
-    "[Q#{color}]"
+    #"[Q#{color}]"
+    @color == :b ? "[♛ ]" : "[♕ ]"
   end
 
 end
@@ -186,7 +199,8 @@ class Bishop < SlidingPiece
   end
 
   def to_s
-    "[B#{color}]"
+    #"[B#{color}]"
+    @color == :b ? "[♝ ]" : "[♗ ]"
   end
 
 end
@@ -200,12 +214,17 @@ b[4, 3] = rook
 rook2 = Rook.new([6, 4], :b, b)
 b[6,4] = rook2
 
+bishop = Bishop.new([2, 4], :b, b)
+b[2, 4] = bishop
+
+knight = Knight.new([0,0], :b, b)
+b[0, 0] = knight
 
 #
 b.render_grid
 #
-piece = Pawn.new([6, 5], :b, b)
-b[6, 5] = piece
+piece = Pawn.new([1, 5], :w, b)
+b[1, 5] = piece
 
 puts
 puts
